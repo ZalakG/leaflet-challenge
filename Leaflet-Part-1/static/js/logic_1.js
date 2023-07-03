cccd10ddd123ace876de80a7d53ab325882cd605
ZalakG <jhalakgajjar@hotmail.com>
2023-07-02 21:40:01 -0400

    code updated

--- Leaflet-Part-1/static/js/logic_1.js
+++ Leaflet-Part-1/static/js/logic_1.js
@@ -1,87 +1,90 @@
-// Store our API endpoint as queryUrl.
-let queryUrl =
-  "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+console.log("logic.js");
 
-// Perform a GET request to the query URL/
-d3.json(queryUrl).then(function (data) {
-  // Once we get a response, send the data.features object to the createFeatures function.
-  createFeatures(data.features);
+let theMap = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 });
 
-// // Creating our initial map object:
-// // We set the longitude, latitude, and starting zoom level.
-// // This gets inserted into the div with an id of "map".
+let myMap = L.map("map", {
+  center: [37.6, -95.665],
+  zoom: 4.6,
+  //layers:[theMap]
+});
 
-// let myMap = L.map("map", {
-//   center: [37.6000, -95.6650],
-//   zoom: 3,
-//   // layers:[ topo, theMap]
-// });
+theMap.addTo(myMap);
 
-// // Adding a tile layer (the background map image) to our map:
-// // We use the addTo() method to add objects to our map.
-// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-// }).addTo(myMap);
+let baseMaps = {
+  "Street": theMap,
+  
+};
 
-function createFeatures(earthquakeData) {
-  // Define a function that we want to run once for each feature in the features array.
-  // Give each feature a popup that describes the place and time of the earthquake.
-  function onEachFeature(feature, layer) {
-    layer.bindPopup(
-      `<h3>${feature.properties.place}</h3><hr><p>${new Date(
-        feature.properties.time
-      )}</p>`
-    );
+// Create an overlay object to hold our overlay.
+let overlayMaps = {
+  Earthquakes: earthquakes
+};
+
+L.control.layers(baseMaps, overlays, { collapsed: flase }).addTo(myMap);
+
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: getColor(feature.geometry.coordinates[2]),
+      color: "#000000",
+      radius: getRadius(feature.properties.mag),
+      stroke: true,
+      weight: 0.5
+    };
   }
 
-  // Create a GeoJSON layer that contains the features array on the earthquakeData object.
-  // Run the onEachFeature function once for each piece of data in the array.
-  let earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature,
-  });
-
-  // Send our earthquakes layer to the createMap function/
-  createMap(earthquakes);
-}
-
-function createMap(earthquakes) {
-  // Create the base layers.
-  let street = L.tileLayer(
-    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-    {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  function getColor(magnitude) {
+    if (magnitude > 90) {
+      return "#ea2c2c";
     }
-  );
+    if (magnitude > 70) {
+      return "#ea822c";
+    }
+    if (magnitude > 50) {
+      return "#ee9c00";
+    }
+    if (magnitude > 30) {
+      return "#eecc00";
+    }
+    if (magnitude > 10) {
+      return "#d4ee00";
+    }
+    return "#98ee00";
+  }
 
-  // let topo = L.tileLayer("https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png", {
-  //   attribution:
-  //     'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
-  // });
+  function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
 
-  // Create a baseMaps object.
-  let baseMaps = {
-    "Street Map": street,
-    //"Topographic Map": topo,
-  };
+    return magnitude * 4;
+  }
 
-  // Create an overlay object to hold our overlay.
-  let overlayMaps = {
-    Earthquakes: earthquakes,
-  };
+  L.geoJson(data, {
+    pointToLayer: function (feature, latlng) {
+      return L.circleMarker(latlng);
+    },
 
-  // Create our map, giving it the streetmap and earthquakes layers to display on load.
-  let myMap = L.map("map", {
-    center: [37.6, -95.665],
-    zoom: 5,
-    layers: [street, earthquakes],
-  });
+    style: styleInfo,
 
-  // Create a layer control.
-  // Pass it our baseMaps and overlayMaps.
-  // Add the layer control to the map.
-  // L.control.layers(baseMaps, overlayMaps, {
-  //   collapsed: false
-  // }).addTo(myMap);
-};
+    onEachFeature: function (feature, layer) {
+      layer.bindPopup(
+        "Magnitude: " +
+          feature.properties.mag +
+          "<br>Depth: " +
+          feature.geometry.coordinates[2] +
+          "<br>Location: " +
+          feature.properties.place
+      );
+    },
+  }).addTo(earthquakes);
+
+  earthquakes.addTo(myMap);
+
+});
+  
