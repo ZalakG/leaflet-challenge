439dc05c1e461eea2886b102112a3af8f0a50ca7
ZalakG <jhalakgajjar@hotmail.com>
2023-07-06 23:56:22 -0400

    final code

--- Leaflet-Part-1/static/js/logic_1.js
+++ Leaflet-Part-1/static/js/logic_1.js
@@ -1,10 +1,12 @@
-console.log("logic_1.js");
+//console.log("logic_1.js");
 
+// Add the tile layer that will be the ackground of the map.
 let theMap = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 });
 
+// Create myMap object
 let myMap = L.map("map", {
   center: [40.7, -94.5],
   zoom: 5,
@@ -13,21 +15,30 @@ let myMap = L.map("map", {
 
 theMap.addTo(myMap);
 
+// Create baseMaps object
 let baseMaps = {
   Street: theMap,
 };
 
+// Initialize the layerGroup
 let earthquakes = new L.LayerGroup();
 
+// Create an overlay object to hold  overlay
 let overlays = {
   Earthquakes: earthquakes,
 };
 
+// Create a layer control.
+// Pass it our baseMaps and overlayMaps.
+// Add the layer control to the map.
 L.control.layers(baseMaps, overlays, { collapsed: false }).addTo(myMap);
 
+// Get the Earthquak GeoJSON data using d3 
 d3.json(
   "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 ).then(function (data) {
+
+  // 
   function styleInfo(feature) {
     return {
       opacity: 1,
@@ -40,25 +51,7 @@ d3.json(
     };
   }
 
-  function getColor(magnitude) {
-    if (magnitude > 90) {
-      return "#ea2c2c";
-    }
-    if (magnitude > 70) {
-      return "#ea822c";
-    }
-    if (magnitude > 50) {
-      return "#ee9c00";
-    }
-    if (magnitude > 30) {
-      return "#eecc00";
-    }
-    if (magnitude > 10) {
-      return "#d4ee00";
-    }
-    return "#98ee00";
-  }
-
+  // Get marker sixe by magnitude
   function getRadius(magnitude) {
     if (magnitude === 0) {
       return 1;
@@ -67,6 +60,7 @@ d3.json(
     return magnitude * 4;
   }
 
+  // Create a GeoJson layer containing the features array
   L.geoJson(data, {
     pointToLayer: function (feature, latlng) {
       return L.circleMarker(latlng);
@@ -87,37 +81,48 @@ d3.json(
   }).addTo(earthquakes);
 
   earthquakes.addTo(myMap);
-
-  // Create map legend to provide context for map data
-  let legend = L.control({ position: "bottomright" });
-
-  legend.onAdd = function () {
-    var div = L.DomUtil.create("div", "info legend");
-    var grades = [10, 30, 50, 70, 90];
-    var labels = [];
-    var legendInfo = "<h4>Magnitude</h4>";
-
-    div.innerHTML = legendInfo;
-
-    // go through each magnitude item to label and color the legend
-    // push to labels array as list item
-    for (var i = 0; i < grades.length; i++) {
-      labels.push(
-        '<ul style="background-color:' +
-          chooseColor(grades[i] + 1) +
-          '"> <span>' +
-          grades[i] +
-          (grades[i + 1] ? "&ndash;" + grades[i + 1] + "" : "+") +
-          "</span></ul>"
-      );
-    }
-
-    // add each label list item to the div under the <ul> tag
-    div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-
-    return div;    
-  };
-
-  //legend.addTo(myMap);
-
 });
+
+// Create map legend to provide context for map data
+let legend = L.control({ position: "bottomright" });
+
+legend.onAdd = function () {
+  let div = L.DomUtil.create("div", "info legend");
+  labels = ["<strong>Depth</strong>"];
+  depth = [-10, 10, 30, 50, 70, 90];
+
+  for (var i = 0; i < depth.length; i++) {
+    div.innerHTML += labels.push(
+      '<i class="circle" style="background:' +
+        getColor(depth[i]) +
+        '"></i> ' +
+        (depth[i] ? depth[i] : "+")
+    );
+  }
+  div.innerHTML = labels.join("<br>");
+  return div;
+};
+
+// Get marker color by depth
+function getColor(depth) {
+  if (depth > 90) {
+    return "#ea2c2c";
+  }
+  if (depth > 70) {
+    return "#ea822c";
+  }
+  if (depth > 50) {
+    return "#ee9c00";
+  }
+  if (depth > 30) {
+    return "#eecc00";
+  }
+  if (depth > 10) {
+    return "#d4ee00";
+  }
+  return "#98ee00";
+}
+
+legend.addTo(myMap);
+
+earthquakes.addTo(myMap);
